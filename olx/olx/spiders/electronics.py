3da0b50d8867cdd2299eccbfaf705b0532e492d6
TORAN <toran.sahu@yahoo.com>
2017-10-24 21:10:53 +0530

    docstring added

--- olx/olx/spiders/electronics.py
+++ olx/olx/spiders/electronics.py
@@ -1,11 +1,16 @@
+"""OLX Crawler."""
 # -*- coding: utf-8 -*-
+
 import scrapy
 
 
 class ElectronicsSpider(scrapy.Spider):
+    """Crawl electronics section."""
+
     name = 'electronics'
     allowed_domains = ['www.olx.in']
     start_urls = ['http://www.olx.in/']
 
     def parse(self, response):
+        """Parse the content of the page."""
         pass
