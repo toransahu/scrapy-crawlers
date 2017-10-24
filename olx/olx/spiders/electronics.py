f2dee8f84fd8ffaf25658944095f6aa22d22a512
TORAN <toran.sahu@yahoo.com>
2017-10-24 21:46:17 +0530

    'rule' property added using 'LinkExtractor' and 'Rule'

--- olx/olx/spiders/electronics.py
+++ olx/olx/spiders/electronics.py
@@ -2,18 +2,35 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class ElectronicsSpider(CrawlSpider):
-    """Crawl many pages of electronics section using CrawlSpider class."""
+    """
+    Crawl many pages of electronics section using CrawlSpider class.
+
+    CrawlSpider: This class makes crawling many pages of a site easier.
+
+    """
 
     name = 'electronics'
     allowed_domains = ['www.olx.in']
-    start_urls = ['https://www.olx.in/tv-video-audio/',
-                  'https://www.olx.in/computers-accessories/',
-                  'https://www.olx.in/games-entertainment/'
-                  ]
+    start_urls = [
+        'https://www.olx.in/tv-video-audio/',
+        'https://www.olx.in/computers-accessories/',
+        'https://www.olx.in/games-entertainment/'
+    ]
+    """
+    Rules of navigating the site.
 
+    LinkExtractor: actually takes parameters to draw navigation boundaries.
+    restrict_css parameter: to set the class for NEXT page. Use inspect element
+                            to find the class.
+    """
+    rules = (Rule(
+        LinkExtractor(allow=(), restrict_css=('.pageNextPrev', )),
+        callback="parse_item",
+        follow=True, ))
 
     def parse(self, response):
         """Parse the content of the page."""
